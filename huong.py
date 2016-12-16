192510b58809d96a287e0840605f96843c350685
Phat Nguyen <mphat10@gmail.com>
2016-12-16 14:05:09 +0700

    set default and no lib

--- huong.py
+++ huong.py
@@ -1,12 +1,15 @@
-from collections import defaultdict
 def function():
 	words= set(line.strip() for line in open('words.txt'))
-	d=defaultdict(list)
+	d={}
 	for w in words:
 		if len(w) not in d:
 			d[len(w)]=w
 		elif len(w) in d:
-			d[w].append(w)  
+			key = str(len(w))
+			d.setdefault(key, [])
+			d[key].append(w)
+				
+  			
 	return d
 
 print(function())
