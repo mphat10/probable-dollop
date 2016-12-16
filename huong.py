2b8a44cb6be17e6bd001575f3da2f559c18b39b2
Phat Nguyen <mphat10@gmail.com>
2016-12-16 16:44:24 +0700

    d= d+ value

--- huong.py
+++ huong.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
 def function():
 	words= set(line.strip() for line in open('words.txt'))
-	d=defaultdict(list)
+	d=defaultdict()
 	for w in words:
 		if len(w) not in d:
 			d[len(w)]=w
 		elif len(w) in d:
-			d[w].append(w)  
+			d[len(w)]+=w  
 	return d
 
 print(function())
