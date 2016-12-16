724635cc6976d4fde149bc4e9de979934756a39a
Phat Nguyen <mphat10@gmail.com>
2016-12-16 13:29:19 +0700

    use collections.defaultdict (added in Python 2.5). This allows you to specify the default object type of a missing key such as list) So instead of creating a key if it doesn't exist first and then appending to the value of the key, cut out the middle-man and just directly append to non-existing keys to get the desired result

--- huong.py
+++ huong.py
@@ -1,11 +1,12 @@
+from collections import defaultdict
 def function():
 	words= set(line.strip() for line in open('words.txt'))
-	d={}
+	d=defaultdict(list)
 	for w in words:
 		if len(w) not in d:
 			d[len(w)]=w
 		elif len(w) in d:
-			d.update({len(w):w})
+			d[w].append(w)  
 	return d
 
 print(function())
