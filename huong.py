834fd2e6e5d82545a7c81349dd0d89accacc2686
Phat Nguyen <mphat10@gmail.com>
2016-12-16 17:08:39 +0700

    add or replace?

--- huong.py
+++ huong.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
 def function():
 	words= set(line.strip() for line in open('words.txt'))
-	d=defaultdict()
+	d={}
 	for w in words:
 		if len(w) not in d:
 			d[len(w)]=w
-		elif len(w) in d:
-			d[len(w)]+=w  
+		else:
+			d.update({len(w):w})
 	return d
 
 print(function())
